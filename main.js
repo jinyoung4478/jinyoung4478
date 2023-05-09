9c1aa1410d7579ff10e07e063814d2d288850a33
Harry <skswls0@daum.net>
2023-05-09 10:07:35 +0900

    한국 시간대 설정

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const axios = require('axios');
-const moment = require('moment');
 const fs = require('fs');
+const moment = require('moment');
+moment.locale('ko');
 
 // - 강수형태(PTY) 코드 : (초단기) 없음(0), 비(1), 비/눈(2), 눈(3), 빗방울(5), 빗방울눈날림(6), 눈날림(7)
 const precipitationStateList = {
